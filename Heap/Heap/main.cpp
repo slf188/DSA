106c1354f95a8fe84d018a91702a65ac02f41e47
Felipe Vallejo <stephanovallejom@gmail.com>
2021-12-20 15:38:24 -0500

    insert function added

--- Heap/Heap/main.cpp
+++ Heap/Heap/main.cpp
@@ -8,6 +8,20 @@
 #include <iostream>
 using namespace std;
 
+void insert(int A[], int n){
+    int temp, i = n;
+    temp = A[n];
+    while(i > 1 && temp > A[i/2]){
+        A[i] = A[i/2];
+        i = i/2;
+    }
+    A[i] = temp;
+}
+
 int main() {
+    
+    int A[] = {30, 20, 15, 5, 10, 12, 6};
+    insert(A, 40);
+    
     return 0;
 }
