27b0d1a6b56874a0cfd0351db54eed902a377872
Felipe Vallejo <stephanovallejom@gmail.com>
2021-12-20 15:15:44 -0500

    Heap c++ file ready

--- Heap/Heap/main.cpp
+++ Heap/Heap/main.cpp
@@ -6,9 +6,8 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
     return 0;
 }
