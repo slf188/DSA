61ab576b51473e84d34c8df55287874d9529b0bf
Felipe Vallejo <stephanovallejom@gmail.com>
2022-01-06 18:32:55 -0500

    Bubble sort check

--- Sorting Techniques/Sorting Techniques/main.cpp
+++ Sorting Techniques/Sorting Techniques/main.cpp
@@ -8,7 +8,26 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void bubbleSort(int A[], int n){
+    int flag;
+    for(int i = 0; i < n - 1; i++){
+        flag = 0;
+        for(int j = 0; j < n - 1 - i; j++)
+            if(A[j] > A[j + 1]){
+                swap(A[j], A[j + 1]);
+                flag = 1;
+            }
+        /* By introducing flag we can identify whether there was any swapping in the past, if there is no swapping then the list is already sorted and can stop the whole algorithm.
+         By introducing flag we have made bubble sort an adaptive technique, by nature is it not adaptive.
+         */
     
+        if(flag == 0)
+            break;
+    }
+}
+
+int main() {
+    int A[] = {8, 5, 7, 3, 2};
+    bubbleSort(A, 5);
     return 0;
 }
