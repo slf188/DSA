a157c030b0733cdc4abbb161673f165536e2032d
Felipe Vallejo <stephanovallejom@gmail.com>
2022-01-14 00:20:50 -0500

    Shell sort added

--- Sorting Techniques/Sorting Techniques/main.cpp
+++ Sorting Techniques/Sorting Techniques/main.cpp
@@ -321,8 +321,27 @@ void radixSort(int A[], int n){
     cout << endl;
 }
 
+void shellSort(int A[],int n){
+    int gap,i,j,temp;
+    
+    for(gap=n/2;gap>=1;gap/=2){
+        for(i=gap;i<n;i++){
+            temp=A[i];
+            j=i-gap;
+            while(j>=0 && A[j]>temp){
+                A[j+gap]=A[j];
+                j=j-gap;
+            }
+            A[j+gap]=temp;
+        }
+    }
+    for(int i = 0; i < n; i++)
+        printf("%d ",A[i]);
+    cout << endl;
+}
+
 int main() {
-    int A[] = {237, 146, 259, 348, 152, 163, 235, 48, 36, 62};
-    radixSort(A, 10);
+    int A[] = {9, 5, 16, 8, 13, 6, 12, 10, 4, 2, 3};
+    shellSort(A, 11);
     return 0;
 }
