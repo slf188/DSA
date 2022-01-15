0f4688b524975ef62ed9b5266532cb20c65c3829
Felipe Vallejo <stephanovallejom@gmail.com>
2022-01-15 17:20:21 -0500

    Chaining added

--- Hashing Techniques/Hashing Techniques/main.cpp
+++ Hashing Techniques/Hashing Techniques/main.cpp
@@ -6,23 +6,27 @@
 //
 
 #include <iostream>
+#include "Node.h"
 using namespace std;
 
-void chaining(){}
-
-void linearProbing(){
-    
+int hashFunction(int key){
+    return key % 10;
 }
 
-void quadraticProbing(){
-    
-}
-
-void doubleHashing(){
-    
+void insert(struct Node *H[], int key){
+    int index = hashFunction(key);
+    SortedInsert(&H[index],key);
 }
 
 int main() {
+    struct Node *hashTable[10];
+    int i;
     
+    for(i=0;i<10;i++)
+        hashTable[i]=NULL;
+    
+    insert(hashTable,12);
+    insert(hashTable,22);
+    insert(hashTable,42);
     return 0;
 }
