586473a8cbb6522c528a82a415f223cc176bd51a
Felipe Vallejo <stephanovallejom@gmail.com>
2022-01-27 19:28:37 -0500

    BFS added

--- Graphs/Graphs/main.cpp
+++ Graphs/Graphs/main.cpp
@@ -6,12 +6,52 @@
 //
 
 #include <iostream>
+#include <queue>
 using namespace std;
 
-void breadthFirstSearch(){
+void breadthFirstSearch(int vertix, int A[][8], int n){
+    int visited[8] {0};
+    queue<int> q;
+    int u;
+    // Visit the starting vertex
+    cout << vertix;
+    // Mark is as visited
+    visited[vertix] = 1;
+    // Enqueue the vertex in the queue
+    q.emplace(vertix);
+    // Repeat the loop until we have visited all the pending vertices
+    while(!q.empty()){
+        // Take out the vertex from the queue
+        u = q.front();
+        q.pop();
+        // Visit the adjacent vertices
+        for(int v = 1; v<=n; v++)
+            // Check if it has been visited or not
+            if(A[u][v]==1 && visited[v]==0){
+                // Visit and enqueue the vertex
+                cout << v;
+                visited[v] = 1;
+                q.emplace(v);
+            }
+    }
+    cout << endl;
 }
 
 int main() {
+    int A[8][8] = {{0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 1, 1, 1, 0, 0, 0},
+        {0, 1, 0, 1, 0, 0, 0, 0},
+        {0, 1, 1, 0, 1, 1, 0, 0},
+        {0, 1, 0, 1, 0, 1, 0, 0},
+        {0, 0, 0, 1, 1, 0, 1, 1},
+        {0, 0, 0, 0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0, 1, 0, 0}};
+    
+    cout << "Vertex: 1 -> " << flush;
+    breadthFirstSearch(1, A, 8);
+    
+    cout << "Vertex: 4 -> " << flush;
+    breadthFirstSearch(4, A, 8);
     
     return 0;
 }
